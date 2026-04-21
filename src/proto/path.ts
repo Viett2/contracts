a0c395f635897ac1341bf43cbfd88b8f0f8d9444
HoangViet2003 <nguyenhoangviet10d1@gmail.com>
2026-04-21 14:42:10 +0700

    feat: add movie and category proto

--- src/proto/path.ts
+++ src/proto/path.ts
@@ -3,5 +3,7 @@ import { join } from "path";
 export const PROTO_PATH = {
   AUTH: join(__dirname, "../../proto/auth.proto"),
   ACCOUNT: join(__dirname, "../../proto/account.proto"),
-  USERS: join(__dirname, "../../proto/users.proto")
+  USERS: join(__dirname, "../../proto/users.proto"),
+  MOVIE: join(__dirname, "../../proto/movie.proto"),
+  CATEGORY: join(__dirname, "../../proto/category.proto"),
 } as const;
